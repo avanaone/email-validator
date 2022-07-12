4c2bcf6a8627fd531142d119dafc6365c3e56515
Didik Hadumi Setiaji <dhs0223@gmail.com>
2022-07-13 03:26:07 +0700

    Adding debounce

--- src/Validator.php
+++ src/Validator.php
@@ -3,18 +3,78 @@ namespace EmailValidator;
 
 class Validator {
     public static $blacklistedDomains;
+    public static $debounceApiKey;
+    private $rulesets = [
+        'by_email_domain' => 'validateByEmailDomain'
+    ];
+    public static $usedRule = [
+        'by_email_domain'
+    ];
+    private $useDebounce = false;
 
     /* @description
      * validate email if it is not inside blacklisted domains
      * @return boolean
      */
     public function validate($email) {
+        if ($this->useDebounce) {
+            $urlValidate = "https://api.debounce.io/v1/?api=".self::$debounceApiKey."&email=".$email;
+            $headers = array(
+                'Accept: application/json',
+                'Content-Type: application/json'
+            );
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $urlValidate);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $return = curl_exec($ch);
+            return $return;
+        } else {
+            $status = 0;
+            foreach(self::$usedRule as $ruleset) {
+                if(($this)->{$this->rulesets[$ruleset]}($email)) {
+                    $status = $status + 1;
+                }
+            }
+
+            $return = json_encode(array(
+                'success' => "$status"
+            ));
+
+            return $return;
+        }
+    }
+
+    /* @description
+     * add rule by key
+     * @return boolean
+     */
+
+    public function ruleConfig($arrayOfRule) {
+       return array_push(self::$usedRule, $arrayOfRule); 
+    }
+
+    /* @description
+     * validate email by domain name
+     * @return boolean
+     */
+    private function validateByEmailDomain($email) {
         $domain = explode('@', $email)[1];
         if (in_array($domain, self::$blacklistedDomains)) { 
             return false;
         } else {
             return true;
         }
+    }
+
+    /* @description
+     * validate email by 3rd party debounce
+     * @return boolean
+     */
+
+    public function useDebounce() {
+        $this->useDebounce = true;
     }
 
     /* @description
