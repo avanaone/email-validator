2733a88d97ab0ff40eda0323a9963764c15f27f7
Didik Hadumi Setiaji <dhs0223@gmail>
2022-10-03 15:44:54 +0700

    Update Debounce

--- src/Validator.php
+++ src/Validator.php
@@ -1,20 +1,101 @@
 <?php
 namespace EmailValidator;
+use Exception;
 
 class Validator {
     public static $blacklistedDomains;
+    protected $email;
+    protected $username;
+    protected $domain;
+    protected $debounceApiKey;
+    protected $debounceHost = "https://api.debounce.io/v1/";
 
     /* @description
-     * validate email if it is not inside blacklisted domains
+     * Set debounceApiKey
+     * @return string $debounceApiKey 
+     */
+    public function setDebounceApiKey($debounceApiKey) {
+        return $this->debounceApiKey = $debounceApiKey;
+    }
+
+    /* @description
+     * Set debounceHost for Mock API Test 
+     * @return string $debounceHost
+     */
+    public function setDebounceHost($debounceHost) {
+        return $this->debounceHost = $debounceHost;
+    }
+
+    /* @description
+     * Validate email use debounce Integration
+     * @return array $result
+     */
+
+    public function validateDebounce($email = null) {
+        if (!$this->email || !$this->debounceApiKey) throw new Exception("Email and API Key must be set");
+
+        $client = new \GuzzleHttp\Client();
+        $urlDebounce = $this->debounceHost . "?api=" . $this->debounceApiKey . "&email=" . $this->email; 
+        try {
+            $response = $client->request('GET', $urlDebounce, [
+                'headers' => [
+                    'accept' => 'application/json',
+                ],
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+        }
+
+        return $response->getBody();
+    }
+
+    /* @description
+     * Internally validate email by blacklisted domains array
+     * Optional, should use db instead
      * @return boolean
      */
-    public function validate($email) {
-        $domain = explode('@', $email)[1];
-        if (in_array($domain, self::$blacklistedDomains)) { 
-            return false;
+    public function validate($email = null) {
+        $domain = explode('@', $email ? $email : $this->email)[1];
+        return in_array($domain, self::$blacklistedDomains);
+    }
+
+    /* @description
+     * Set Email string
+     * @return void 
+     */
+
+    public function setEmail($email) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->email = $email;
+            $this->domain = explode('@', $email)[1];
+            $this->username = explode('@', $email)[0];
         } else {
-            return true;
+            throw new Exception("The email string is invalid");
         }
+    }
+
+    /* @description
+     * Get Email string
+     * @return string $email
+     */
+    public function getEmail() {
+        return $this->email;
+    }
+
+    /* @description
+     * Get Domain from Email string
+     * @return string $domain
+     */
+    public function getDomain() {
+        return $this->domain;
+    }
+
+    /* @description
+     * Get Username string from Email string
+     * @return string $username
+     */
+    public function getUsername() {
+        return $this->username;
     }
 
     /* @description
